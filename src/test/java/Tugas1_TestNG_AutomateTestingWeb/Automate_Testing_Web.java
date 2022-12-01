e9834976eec0b177a09eb696499126f6185951c5
tangkaswahyu75 <tangkaswahyu75@gmail.com>
2022-12-01 23:25:17 +0800

    Tugas Challenge 6

---
+++ src/test/java/Tugas1_TestNG_AutomateTestingWeb/Automate_Testing_Web.java
@@ -0,0 +1,4 @@
+package Tugas1_TestNG_AutomateTestingWeb;
+
+public class Automate_Testing_Web {
+}
---
+++ src/test/java/Tugas2_TestNG_RestAssuredReqres/TangkasWahyu_RestAssured.java
@@ -0,0 +1,128 @@
+package Tugas2_TestNG_RestAssuredReqres;
+
+import io.restassured.RestAssured;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.hamcrest.Matchers;
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.baseURI;
+
+public class TangkasWahyu_RestAssured {
+    String baseUrl = "https://reqres.in/api/users";
+
+    @Test(priority = 1)
+    public void testGetRequest1() {
+        baseURI = baseUrl;
+
+        RestAssured.given()
+                .header("Content-Type", "application/json")
+                .queryParam("id", 7)
+                .get(baseURI +"?page=2")
+                .then()
+                .assertThat()
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schema/get.json"))
+                .statusCode(200)
+                .body("page",Matchers.equalTo(2))
+                .log()
+                .all();
+    }
+
+    @Test(priority = 2)
+    public void testGetRequest2() {
+        baseURI = baseUrl;
+        RestAssured.given()
+                .header("Content-Type", "application/json")
+                .queryParam("id", 7)
+                .get(baseURI +"?page=2")
+                .then()
+                .assertThat()
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schema/get.json"))
+                .statusCode(200)
+                .body("data.id", Matchers.equalTo(7))
+                .body("data.email", Matchers.equalTo("michael.lawson@reqres.in"))
+                .body("data.first_name", Matchers.equalTo("Michael"))
+                .body("data.last_name", Matchers.equalTo("Lawson"))
+                .log()
+                .all();;
+    }
+
+    @Test(priority = 3)
+    public void testPostRequest() {
+        JSONObject bodyJson = new JSONObject();
+        baseURI = baseUrl;
+
+        bodyJson.put("name", "TangkasWahyu");
+        bodyJson.put("job", "Associate QA Engineer");
+
+        RestAssured.given()
+                .header("Content-Type", "application/json")
+                .body(bodyJson.toString())
+                .post(baseURI)
+                .then()
+                .assertThat()
+                .statusCode(201)
+                .body("name", Matchers.equalTo("TangkasWahyu"))
+                .body("job", Matchers.equalTo("Associate QA Engineer"))
+                .log()
+                .all();
+    }
+
+    @Test(priority = 4)
+    public void testPutRequest() {
+        JSONObject bodyJson = new JSONObject();
+        baseURI = baseUrl;
+
+        bodyJson.put("name", "TangkasWahyu");
+        bodyJson.put("job", "Associate QA Engineer");
+
+        RestAssured.given()
+                .header("Content-Type", "application/json")
+                .body(bodyJson.toString())
+                .put(baseURI + "/2")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("name", Matchers.equalTo("TangkasWahyu"))
+                .body("job", Matchers.equalTo("Associate QA Engineer"))
+                .log()
+                .all();
+    }
+
+    @Test(priority = 5)
+    public void testPatchRequest() {
+        JSONObject bodyJson = new JSONObject();
+        baseURI = baseUrl;
+
+        bodyJson.put("name", "TangkasWahyu");
+        bodyJson.put("job", "Associate QA Engineer");
+
+        RestAssured.given()
+                .header("Content-Type", "application/json")
+                .body(bodyJson.toString())
+                .patch(baseURI + "/2")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("name", Matchers.equalTo("TangkasWahyu"))
+                .body("job", Matchers.equalTo("Associate QA Engineer"))
+                .log()
+                .all();
+    }
+
+    @Test(priority = 6)
+    public void testDeleteRequest() {
+        baseURI = baseUrl;
+
+        RestAssured.given()
+                .header("Content-Type", "application/json")
+                .delete(baseURI + "/2")
+                .then()
+                .assertThat()
+                .statusCode(204)
+                .body("data", Matchers.equalTo("2"))
+                .body("message", Matchers.containsString("Successfully!"))
+                .log()
+                .all();;
+    }
+}
