1fb6999169264ee3ad980d708409bb05ee7c4e30
Andreanov Ridhovan <andreanovr@gmail.com>
2022-12-01 19:42:37 +0700

    Submit Challenge Chapter 6

--- src/test/java/config.java
+++ src/test/java/config.java
@@ -3,7 +3,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
 import java.util.concurrent.TimeUnit;
 
 public class config {
